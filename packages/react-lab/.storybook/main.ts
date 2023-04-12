ef7689bb3e1ce1cda19e9d3f19d92b132c83345a
hwookim <gusdn9754@gmail.com>
2023-04-12 22:12:45 +0900

    chore: Migrate CRA to vite

--- packages/react-lab/.storybook/main.ts
+++ packages/react-lab/.storybook/main.ts
@@ -1,15 +1,19 @@
-module.exports = {
+import type { StorybookConfig } from '@storybook/react-vite';
+const config: StorybookConfig = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
-    'storybook-addon-react-router-v6',
     'my-addon',
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
   },
 };
+export default config;
